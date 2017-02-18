d1f2e9578cb89832d08907ab5081e0aec26ce81c
xingyunbo <wangbo_zju@126.com>
2017-02-18 23:40:03 +0800

    dorm

--- MyCode/ch03/ex3_4uva455.cpp
+++ MyCode/ch03/ex3_4uva455.cpp
@@ -8,28 +8,29 @@ const int maxn = 85;
 int main()
 {
 
-//	freopen("test.in", "r", stdin);
-//	freopen("test.out", "w", stdout);
+	freopen("test.in", "r", stdin);
+	freopen("test.out", "w", stdout);
 
 	int T;
 	scanf("%d", &T);
-//while(T--) {
-	char str[maxn];
-	scanf("%s", str);
-	int period;
-	for (period = 1; period <= strlen(str); period++) {
-		int flag = 1;
-//		printf("strlen(str) = %d, flag = %d\n", strlen(str), flag);
-//		printf("period = %d\n", period);
-		if ( !(strlen(str) % period) ) { // if1 
-			for (int i = 0; i < strlen(str) - period; i++) {
-//				printf("(%d) period = %d, str[%d] = %c.\n", i, period, i, str[i]);
-				if (str[i+period] != str[i]) {flag = 0; break;}
-			}
+	while(T--) {
+		char str[maxn];
+		scanf("%s", str);
+		int period;
+		for (period = 1; period <= strlen(str); period++) {
+			int flag = 1;
+//			printf("strlen(str) = %d, flag = %d\n", strlen(str), flag);
+//			printf("period = %d\n", period);
+			if ( !(strlen(str) % period) ) { // if1 
+				for (int i = 0; i < strlen(str) - period; i++) {
+//					printf("(%d) period = %d, str[%d] = %c.\n", i, period, i, str[i]);
+					if (str[i+period] != str[i]) {flag = 0; break;}
+				}
 		
-			if (flag) { printf("%d\n", period); break;} // 在if1里面 
+				if (flag) { printf("%d\n", period); break;} // 在if1里面 
+			}
 		}
+		if (T > 0) printf("\n");
 	}
-//}
 	return 0;
 }
