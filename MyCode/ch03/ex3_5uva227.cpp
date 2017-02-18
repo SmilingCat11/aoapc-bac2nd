dd7fd57464fb4c16aa4b2a772ed728a06bdbd57a
SmilingCat11 <smilingcat11@163.com>
2017-02-18 21:56:09 +0800

    lab

--- MyCode/ch03/ex3_5uva227.cpp
+++ MyCode/ch03/ex3_5uva227.cpp
@@ -1,64 +1,83 @@
 // ex3_5uva227 -- Puzzle
-#include <stdio.h>
+// 使用while(```getchar())时里尽量不再用getchar()，易出匪夷所思的bug
+// 也不要再用循环，因为控制字符读取的只在最外面的while里
+// 尽可能只一次读取字符
+// 数组间如何交换，swap(s[i][j], s[i++][j])是否合理
+// 二维数组如何全置零
 
-void swap(char& x, char& y) {
+#include <stdio.h>
+#include <string.h>
+
+void swap(char * x, char * y) {
 	char t;
-	x = t;
-	x = y;
-	y = t;
+	t = *x;
+	*x = *y;
+	*y = t;
 }
 
 int main()
 {
-	char s[5][6] = {};
+	freopen("test.in", "r", stdin);
+	freopen("test.out", "w", stdout);
+	char s[6][6] = {};
 	int c;
+	int fi = 0, fj = 0;
+	int i = 0, j = 0;
+	int count = 0; // the ith test
+	int flag = 1; // decide whether there is a wrong move
+	
 	while ((c = getchar()) != 'Z') {
-		int fi = 0, fj = 0;
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				s[i][j] = getchar();
-printf("%d ", s[i][j]);
-				if (s[i][j] == ' ') {fi = i; fj = j;}
+		if (i <= 4) {
+			if (c != '\n') {
+				if (c == ' ') {fi = i; fj = j;}
+				s[i][j++] = c;
 			}
-getchar();
-printf("\n");
+			else {j = 0; i++;}
 		}
-		int oper, flag = 1;
-		while (flag && (oper = getchar()) != '0') {
-			switch (oper) {
+
+		if (i > 4 && c != '0') {
+			if (flag) { switch (c) {
 				case 'A':
-					if (fi>0) swap(s[fi][fj], s[--fi][fj]);
+					if (fi>0) { swap(&s[fi][fj], &s[fi-1][fj]); fi--; }
 					else flag = 0;
 					break;
 				case 'B':
-					if (fi<5) swap(s[fi][fj], s[++fi][fj]);
+					if (fi<5) { swap(&s[fi][fj], &s[fi+1][fj]); fi++; }
 					else flag = 0;
 					break;
 				case 'L':
-					if (fj>0) swap(s[fi][fj], s[fi][--fj]);
+					if (fj>0) { swap(&s[fi][fj], &s[fi][fj-1]); fj--; }
 					else flag = 0;
 					break;
 				case 'R':
-					if (fj<5) swap(s[fi][fj], s[fi][++fj]);
+					if (fj<5) { swap(&s[fi][fj], &s[fi][fj+1]); fj++; }
 					else flag = 0;
 					break;
 				default:
 					break;
 			}
-		}
-		getchar();
-		int count = 0;
-		printf("Puzzle #%d:\n", ++count);
-		for (int i = 0; i < 5; i++) {
-			int first = 1;
-			for (int j = 0; j < 6; j++) {
-				if (first) first = 0;
-				else printf(" ");
-				printf("%c", s[i][j]);
 			}
-			printf("\n");
 		}
-		printf("\n");
+
+		if (i > 4 && c == '0') {
+			printf("Puzzle #%d:\n", ++count);
+			if (flag) { 
+				for (int i = 0; i < 5; i++) {
+					int first = 1;
+					for (int j = 0; j < 5; j++) {
+						if (first) first = 0;
+						else printf(" ");
+						printf("%c", s[i][j]);
+					}
+				printf("\n");
+				}
+			printf("\n");
+			}
+			else printf("This puzzle has no final configuration.\n\n");
+			i = 0; j = 0;  // initialation
+			fi = 0; fj = 0;
+			flag = 1; // initialation
+		}
 	}
 	return 0;
 }
